4d4d9fbe71073d54c0c3fb31c3e4296367b155b2
JRezCode <jonathan.reznik@gmail.com>
2022-06-20 23:20:36 -0700

    board and moving pieces

--- eight-queens/PyGame8Queen.py
+++ eight-queens/PyGame8Queen.py
@@ -3,24 +3,40 @@ from qt import QtGui, QtWidgets, QtCore, _enum, _exec
 # python 8 queens problem
 
 class Board():
-    def __init__(self, grid_size):
+    def __init__(self, grid_size, game_type):
+        global grid
+        grid = grid_size
+        self.game = game_type
         global board
         board = self.board_structure(grid_size)
-        self.grid_size = grid_size
 
     def board_structure(self,size):
         brd = []
-        file = 'A'
         for i in range(size):
             brd.append([])
-            rank = 1
-            if i > 0:
-                file = chr(ord(file) + 1)
             for j in range(size):
-                if j > 0:
-                    rank += 1
-                brd[i].append(Chesssquare(file,str(rank)))
+                brd[i].append(None)
         return brd
+
+    @staticmethod
+    def make_chess_board():
+        for i in range(grid):
+            for j in range(grid):
+                board[i][j] = (Chesssquare(i,j))
+
+    # def board_structure(self,size):
+    #     brd = []
+    #     file = 'A'
+    #     for i in range(size):
+    #         brd.append([])
+    #         rank = 1
+    #         if i > 0:
+    #             file = chr(ord(file) + 1)
+    #         for j in range(size):
+    #             if j > 0:
+    #                 rank += 1
+    #             brd[i].append(Chesssquare(file,str(rank)))
+    #     return brd
 
     # TODO: need to work on this function for algorithm for checking solution goes here
     # separate functional solutions for different algorithms such as backtracking and brute force
@@ -34,69 +50,88 @@ class Chesssquare():
     def __init__(self, file, rank):
         self.file = file
         self.rank = rank
-        self.piece = None
+        self.piece = Gamepiece(None,file,rank)
 
     def __str__(self):
-        if self.piece is not None:
-            return "%s\t" % (self.piece)
-        else:
-            return "%s%s\t" % (self.file, self.rank)
+        return "%s%s" % (self.file, self.rank) + ":" + "%s\t" % (self.piece)
 
     # def __str__(self,debug):
     #     return "%s%s-%s" % (self.file, self.rank, self.piece)
-    
+
 
 class Gamepiece():
     def __init__(self, type, file, rank):
         self.piece_type = type
-        self.piece_location = self.place_piece(file, rank)
-    
-    def place_piece(self, file, rank):
-        board[file][rank].piece = self
-        return file,rank
+        self.piece_location = (file,rank)
+        # self.place_piece(file, rank)
 
-    def at_square(self):
-        return ''.join(map(str,self.piece_location))
+#        return file,rank
+    def place_piece(self, file, rank, type):
+        board[file][rank].piece = Gamepiece(type,file,rank)
+        self.piece_location = (file,rank)
+
+    def reset_piece(self):
+        file = self.piece_location[0]
+        rank = self.piece_location[1]
+        board[file][rank].piece.piece_type = None
+
+    def show_square(self):
+        return str(self) + '@' + ''.join(map(str,self.piece_location))
+
+    def move_piece(self, x,y):
+        if self.islglmove(x,y):
+            temp = self
+            self.reset_piece()
+            self.place_piece(x,y,temp.piece_type)
 
     def __str__(self):
+        if self.piece_type is None:
+            return "N"
         return self.piece_type
 
 class Queen(Gamepiece):
     def __init__(self,f,r):
         super().__init__("Qu",f,r)
+        self.place_piece(f,r,"Qu")
     
-    # def move_piece():
-
-    # def lglmove():
-
+    def islglmove(self,x,y):
+        if self.piece_location[0] is x or self.piece_location[1] is y:
+            return True
+        elif x - self.piece_location[0] is y - self.piece_location[1]:
+            return True
+        else:
+            return False
 
 class Game(Board):
-    def __init__(self, size, type):
-        super().__init__(size)
-        self.game = type
-        board = self.board_structure(size)
+    def __init__(self, g_size, g_type):
+        super().__init__(g_size,g_type)
+        if g_type == "queens": 
+            board = self.make_chess_board()
 
     def __repr__(self):
-        my_board_repr = ""
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                 my_board_repr += str(board[i][j])
+        my_board_repr = "Board:\n"
+        for i in range(grid):
+            for j in range(grid):
+                my_board_repr += str(board[i][j])
+                if j is (grid - 1):
+                    my_board_repr += "\n"
                 #  "this is __repr__"
         return my_board_repr
 
 def main():
     b = Game(4,"queens")
-    p1 = Queen(0,0)
-    p2 = Queen(2,3)
-
+    q1 = Queen(2,0)
+    q2 = Queen(0,1)
+    q3 = Queen(3,2)
+    q4 = Queen(1,3)
+    q2.move_piece(2,1)
+    q2.move_piece(0,1)    
     # user input/output for game start
     #choice = input()
     # board = new_game(game_type,game_difficulty_interactive)
     # b.place_queen(1,1)
     # b.place_queen(0,0)
     print(b)
-    print(p1.at_square())
-    print(p2.at_square())
     # print(b.board)
     # print(b.board[0])
 
