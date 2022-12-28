8f62d6f7fd6ca7ec274ef4ed80ed1e1d032bd18e
Jonathan Reznik <22972886+jonathanreznik1@users.noreply.github.com>
2022-12-27 16:48:19 -0800

    Changes to work

--- eight-queens/Queens-final.py
+++ eight-queens/Queens-final.py
@@ -1,17 +1,29 @@
 import uuid
 import sys
 
-# TODO:
-# 1) Finish commenting and check file/rank row /col using paper-pencil
-# 2) Add masking of paths to algorithm that solves
-# 3) Add program graphics and interactive moves
+
+'''
+This program will be able to solve an N queens problem using a backtracking
+algorithm that is basically somewhat brute force.  There is no solution masking
+in the algorithm.  It is capable of calculating up to 19 queens in a reasonable
+length of time, and then it becomes unmanageable haha.
+'''
 
 
 # Flags
-DEBUG = True
+DEBUG = False
 CLI_MODE = True
-SOLVE = 1
+
+# Other Input
+BOARDS = (10,12,14)   #enter a tuple here of the boards to solve
+# NOTE: The order of solutions found will be the same each time so rather than
+# include the same number twice (for now!) use the next param to alter the 
+# number of solutions attempted. 
+SOLVE = (9,2,1)           #enter the max number of solutions to find in each board
            
+# TODO:
+# Add masking paths in algorithm and other efficiencies
+# Create more interactive user input/output
 
 class Game():
     """ Class with a dictionary and static methods to create board games """
@@ -59,38 +71,39 @@ class Square():
         """ Removes object in square . Ret: None """
         self.piece = None
         
-    def __str__(self):
-        """ String override method. Ret: String """
-        rank, file = self.location      # grab rank and file
-        if not self.has_piece():
-            # square is unoccupied by a piece
-            return ""
-        # square has piece output
-        return "%s%s" % (rank, file) + ":" + "Qu\t"
+    # def __str__(self):
+    #     """ String override method. Ret: String """
+    #     rank, file = self.location      # grab rank and file
+    #     if not self.has_piece():
+    #         return ""
+    #     return "Qu"
 
     def __repr__(self):
         """ Representation override method. Ret: String """
+        rank, file = self.location
         if self.has_piece():
             # unoccupied square
-            return "Queen" + chr(ord("A")+self.location[1]) + "@" + str(self.location[1]) + str(self.location[0])
-        return "Square@" + str(self.location[1]) + str(self.location[0])
+            return "QU@" + chr(ord("A")+file) + str(rank+1)
+        return "sq@" + chr(ord("A")+file) + str(rank+1)
 
 
 class Board():
     """ Class for new boards """
     
     # new board gets assigned a uuid and dimension
-    id = uuid.uuid1()
     solutions = {}              #to store solutions
         
     def __init__(self, dims):
         """ Constructor method for the board. Ret: None 
-        Boards have a unique ID and dimension assigned there is also
-        a fixed number of solutions that once found are entered into the
-        class dictionary stored in class var "solutions"."""
+        Boards have a unique ID and dimension assigned
+        There is a fixed number of solutions to each one.
+        Once found entered into "solutions" map."""
+        self.id = uuid.uuid1()
         self.b_dims = dims
+        self.b_index = BOARDS.index(dims)
+        
         self.b_solution_count = 0
-        self.b_solved = False
+        
         self.fresh_board()
         if DEBUG:
             print("new board: " + str(self.id))       
@@ -99,30 +112,31 @@ class Board():
     def fresh_board(self):
         self.brd = [[Square(self,i,j) for i in range(self.b_dims)] for j in range(self.b_dims)]
         self.MOVES = 0
+        self.b_solved = False
         if DEBUG:
             print("board cleared: " + str(self.id))
                 
         
     @staticmethod
-    def isSafe(brd, col, row):
+    def isSafe(brd, row, col):
         """ The method to check conflicts in squares. Ret: None """        
         # check within file for another piece
         for i in range(col):
 #            print(row,i)
-            sq = brd.brd[i][row]
+            sq = brd.brd[row][i]
             if sq.has_piece():
                 return False
         # check along diag        
         for i, j in zip(range(row, -1, -1),
                         range(col, -1, -1)):
-            print(j,i)
+#            print(j,i)
             sq = brd.brd[i][j]
             if sq.has_piece():
                 return False
         # check along other diag
         for i, j in zip(range(row, len(brd.brd), 1),
                         range(col, -1, -1)):
-            print(j,i)
+#            print(j,i)
             sq = brd.brd[i][j]
             if sq.has_piece():
                 return False
@@ -134,15 +148,22 @@ class Board():
         global SOLVE
         i = 0
         
-        while i < SOLVE:
+        while i < SOLVE[board.b_index]:
             if Board.SolveRemaining(board, 0) == False:
                 print("No solution")
                 return False
+
+            # print solved out            
+            board.b_solved = True
+            if CLI_MODE:
+                print(board)
+
+            # store in the solutions object
+            Board.solutions[str(board.b_dims)+str(i)]=Board.recordsolution(board)
             
-            Board.solved(i,board)
-            
-            board.fresh_board()
-            board.reset_move_count()
+            if i < SOLVE[board.b_index] -1:
+                board.fresh_board()
+                board.reset_move_count()
             i+=1
         # return board.b_solved  
         
@@ -151,37 +172,31 @@ class Board():
     @staticmethod
     def SolveRemaining(board,col):
         """ Recursive method for solving the n queens board. Ret: Boolean """  
-        #global MOVES
         if col >= board.b_dims:
             if Board.visited(board):
                 return False
             return True
         
         for row in range(board.b_dims):
-            # Board.add_move(board)
-            if Board.isSafe(board, col, row):
+            Board.add_move(board)
+            if Board.isSafe(board, row, col):
                 square = board.brd[row][col]
                 square.set_queen()
-                # while SOLVE_COUNT < 10:
-                    # if board.found_already():
-                        #  print("hello")
-                        #  continue
                 if Board.SolveRemaining(board, col + 1) == True:
                     return True
                 square.reset()
         return False
 
     def solved(sol,brd):
+        """Helper method for debug mode"""
         print("Solved another board")
-            #register the board solution using a better method
-        Board.solutions[sol]=brd.get_solution()
         print(Board.solutions[sol])
 
 
     @staticmethod
     def matches(board1,board2):
         """ Board 2 is an existing board and Board 1 is the new board """
-        comp = Board.get_solution(board1)
+        comp = Board.recordsolution(board1)
         for [i,j] in comp:
             if [i,j] not in board2:
                 return False
@@ -189,6 +204,7 @@ class Board():
 
     @staticmethod
     def visited(board):
+        """Determines if a board is being revisited"""
         if len(board.solutions)==0:
             return False
         for solved in board.solutions.values():
@@ -196,103 +212,90 @@ class Board():
                 return True
         return False
 
-    def get_solution(board):
-        s = []
+    @staticmethod
+    def recordsolution(board):
+        """Enters a configuration of queens into the solutions map for that board"""
+        s = []                              #stores solution
         for j in range(board.b_dims):
             for i in range(board.b_dims):
-                if board.brd[i][j].has_piece():
-                    s.append([i,j])
+                if board.brd[j][i].has_piece():
+                    s.append([j,i])         #append file,rank to solution set
         return s
 
     def reset_move_count(board):
+        """Resets the move count"""
         board.MOVES = 0
 
     def add_move(board):
+        """Adds to the move count"""
         board.MOVES += 1
 
     def __repr__(self):
         """ Representation override method. Ret: String """
-        my_repr = ''
-        # if self.b_solved:
-        if self.b_solved:
-            my_repr += "Board Solved in %i moves!\n" % (self.MOVES)
-        if CLI_MODE:
-            my_repr += CLI.ShowBoard(self)
+        my_repr = str(self.b_dims)+'-queens '
         if DEBUG:
             my_repr += "Data Representation:\n" + "".join(map(''.join,str(
                 Game.games[self.id].brd))) + "\n"
+        if self.b_solved:
+            my_repr += "\nBoard Solved in %i moves!\n" % (self.MOVES)
+                    #transpose board to show file/rank A1 in lower left of CLI output
+        if CLI_MODE:
+            for i in range(self.b_dims):
+                my_repr += '\n'
+                # process 
+                for rank in self.brd:
+                    my_repr += str(rank[self.b_dims-i-1]) + '\t'
         return my_repr
 
-
-class CLI():
-    """ Class of static methods for transforming boards to pretty CLI output"""
-
-    @staticmethod
-    def ShowBoard(board):
-        """ Static method to print boards and pieces to cli. Ret: String """
-        chess_repr = "Chessboard (CLI Mode):\n"
-        b_dims = board.b_dims                 #TODO: Fix if isn't square
-        rank = b_dims
-        for i in range(b_dims):
-            file = 'A'
-            if i > 0:
-                rank -= 1
-                chess_repr += "\n"
-            for j in range(b_dims):
-                if j > 0:
-                    file = chr(ord(file) + 1)
-                chess_repr += file + str(rank) +':'+ str(board.brd[i][j])[-3:
-                    -1]+"\t"
-        chess_repr += "\n"  
-        return chess_repr
-
     def ShowSolution(solution):
-        chess_repr = "Chessboard (CLI Mode):\n"
-        b_dims = len(solution)
-        rank = b_dims
-        for i in range(b_dims):
-            file = 'A'
-            if i > 0:
-                rank -= 1
-                chess_repr += "\n"
-            for j in range(b_dims):
-                if j > 0:
-                    file = chr(ord(file)+1)
-                chess_repr += file + str(rank) +':'
-                if [i,j] in solution:
-                    chess_repr += 'Qu'
-                chess_repr += "\t"
-        chess_repr += "\n"
-        return chess_repr
+        """Reads from the solution map and displays summary or CLI output"""
+        summary = solution
+        if CLI_MODE:
+            summary = "Chessboard (CLI Mode):\n"
+            b_dims = len(solution)
+            rank = b_dims
+            for i in range(b_dims):
+                file = 'A'
+                if i > 0:
+                    rank -= 1
+                    summary += "\n"
+                for j in range(b_dims):
+                    if j > 0:
+                        file = chr(ord(file)+1)
+                    summary += file + str(rank) +':'
+                    if [i,j] in solution:
+                        summary += 'Qu'
+                    summary += "\t"
+            summary += "\n"
+        return summary
 
 
 def main():
 
     # new game
     g = Game()
-   
-    # three new boards
-    b1 = Board(8)
-    #b2 = Board(8)
-    #b3 = Board(9)
-    g.new_game(b1)
-    #g.new_game(b2)
-    #g.new_game(b3)
-    
-    # print(g)
-   
-    # solve each of the boards
+
+    # uses the global tuple to setup "the boards"
+    for b in BOARDS:
+        g.new_game(Board(b))
+          
+    # solve each of "the boards"
     for uuid in g.games:
         b = g.fetch_board(uuid)
         Board.Solve(b)
-        print(Board.solutions)
-        for solution in Board.solutions.values():
-            # print(len(solution))
-            print(CLI.ShowSolution(solution))
 
+    # show the summary output
+    for s in Board.solutions.values():
+        print(Board.ShowSolution(s))
 
-    #     print(b)
+    # with debug mode on changes to CLI_MODE to show last solved
+    if DEBUG:
+        global CLI_MODE
+        CLI_MODE = True
+        #print(g)
+        print("\n\nLast used board ",end="")
+        print(b)
 
-
+    
 main()
 
